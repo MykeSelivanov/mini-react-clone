e10e29811dad7416a7f475f264111c1e78fc4169
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-16 23:15:42 -0500

    react clone

--- Component.js
+++ Component.js
@@ -1,8 +1,7 @@
 import { useState } from './Shmeact.js';
 
 export default function Component({ propCount }) {
-    const [count, setCount] = useState(10);
-    const count = 0;
+    const [count, setCount] = useState(0);
     const propCountDoubled = 0;
 
     return `
